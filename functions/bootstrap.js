fcc03423aed962b93b627bde7351c95a795b4ebc
gurmeetgold <50425243+gurmeetgold@users.noreply.github.com>
2026-08-19 00:14:35 -0400

    Export trusted event management functions

--- functions/bootstrap.js
+++ functions/bootstrap.js
@@ -1,4 +1,5 @@
-// Keep the existing production functions untouched and layer new MVP invite
-// functions beside them. Requiring index.js initializes firebase-admin once.
+// Keep existing production functions and layer MVP management/invite functions
+// beside them. Requiring index.js initializes firebase-admin once.
 Object.assign(exports, require("./index"));
 Object.assign(exports, require("./invites"));
+Object.assign(exports, require("./eventManagement"));
