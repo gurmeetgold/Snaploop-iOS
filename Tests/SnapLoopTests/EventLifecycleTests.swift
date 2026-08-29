db64316233514b2665d647aeaea0574fc3e54e62
gurmeetgold <50425243+gurmeetgold@users.noreply.github.com>
2026-08-29 17:32:54 -0400

    Update lifecycle tests for 15-day photo window

--- Tests/SnapLoopTests/EventLifecycleTests.swift
+++ Tests/SnapLoopTests/EventLifecycleTests.swift
@@ -30,44 +30,44 @@ final class EventLifecycleTests: XCTestCase {
         XCTAssertEqual(lifecycleStatus(at: end, start: start, end: end), .active)
     }
 
-    func testGraceAfterEndWithinWindow() {
+    func testPhotoWindowAfterEndWithinFifteenDays() {
         let start = Date(timeIntervalSince1970: 1_000_000)
         let end = start + 5 * day
-        let s = lifecycleStatus(at: end + 2 * day, start: start, end: end)
+        let s = lifecycleStatus(at: end + 10 * day, start: start, end: end)
         XCTAssertEqual(s, .grace)
     }
 
-    func testExpiredPastGrace() {
+    func testExpiredPastFifteenDayPhotoWindow() {
         let start = Date(timeIntervalSince1970: 1_000_000)
         let end = start + 5 * day
-        let s = lifecycleStatus(at: end + 3 * day + 1, start: start, end: end)
+        let s = lifecycleStatus(at: end + 15 * day + 1, start: start, end: end)
         XCTAssertEqual(s, .expired)
     }
 
-    func testGraceEndBoundaryIsInclusive() {
+    func testPhotoWindowEndBoundaryIsInclusive() {
         let start = Date(timeIntervalSince1970: 1_000_000)
         let end = start + 5 * day
-        let graceEnd = end + 3 * day
+        let graceEnd = end + 15 * day
         XCTAssertEqual(lifecycleStatus(at: graceEnd, start: start, end: end), .grace)
         XCTAssertEqual(lifecycleStatus(at: graceEnd + 1, start: start, end: end), .expired)
     }
 
-    func testCanSyncDuringActiveAndGraceOnly() {
+    func testCanSyncDuringActiveAndPhotoWindowOnly() {
         let start = Date(timeIntervalSince1970: 1_000_000)
         let end = start + 5 * day
         let e = event(start: start, end: end)
         XCTAssertFalse(EventLifecycle.canSync(e, clock: FixedClock(start - 1), config: config))
         XCTAssertTrue(EventLifecycle.canSync(e, clock: FixedClock(start + day), config: config))
-        XCTAssertTrue(EventLifecycle.canSync(e, clock: FixedClock(end + day), config: config))
-        XCTAssertFalse(EventLifecycle.canSync(e, clock: FixedClock(end + 4 * day), config: config))
+        XCTAssertTrue(EventLifecycle.canSync(e, clock: FixedClock(end + 10 * day), config: config))
+        XCTAssertFalse(EventLifecycle.canSync(e, clock: FixedClock(end + 16 * day), config: config))
     }
 
-    func testCanDownloadThroughGraceButNotAfter() {
+    func testCanDownloadThroughPhotoWindowButNotAfter() {
         let start = Date(timeIntervalSince1970: 1_000_000)
         let end = start + 5 * day
         let e = event(start: start, end: end)
-        XCTAssertTrue(EventLifecycle.canDownload(e, clock: FixedClock(end + 3 * day), config: config))
-        XCTAssertFalse(EventLifecycle.canDownload(e, clock: FixedClock(end + 3 * day + 1), config: config))
+        XCTAssertTrue(EventLifecycle.canDownload(e, clock: FixedClock(end + 15 * day), config: config))
+        XCTAssertFalse(EventLifecycle.canDownload(e, clock: FixedClock(end + 15 * day + 1), config: config))
     }
 
     // MARK: Validation
